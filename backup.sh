878b884450065be914a7ae5ca2043abb1b7a7c2d
MrSamuelLaw <samuel@breakingthelaw.xyz>
2025-03-02 12:41:06 -0600

    added backup script

---
+++ backup.sh
@@ -0,0 +1,11 @@
+# get the name of the current folder
+sourcedir=$(dirname $(realpath "$0"));
+
+# grab the name of the target directory passed in by the user
+targetdir="${1}/${sourcedir##*/}";
+
+# go into the source directory
+cd $sourcedir;
+
+# run rsync and delete any files that have been deleted locally
+rsync -aP --delete $sourcedir $targetdir;
